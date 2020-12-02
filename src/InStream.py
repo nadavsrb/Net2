fbab49d163a580d096d2a84d81056a7251eb9375
nadavsrb <nadavsrb@gmail.com>
2020-12-02 07:54:07 +0200

    fixing implementation in InStream

--- src/InStream.py
+++ src/InStream.py
@@ -1,4 +1,4 @@
-from socket import socket
+import socket
 
 from IOStream import IOStream
 
@@ -13,13 +13,22 @@ class InStream(IOStream):
 
     def getMassage(self):
         isTimeout = False
-        data = ""
+        data = self._nextData
+        self._nextData = ""
 
         try:
+
             while not (self.EOM in data):
                 self._clSocket.settimeout(self.TIMEOUT_SEC)
                 data += self._clSocket.recv(self.BUFFER_SIZE).decode()
                 self._clSocket.settimeout(None)
+
+            index = data.find(self.EOM) + len(self.EOM)
+
+            if index < len(data):
+                self._nextData = data[index:]
+
+            data = data[:index]
 
         except socket.timeout:
             isTimeout = True
