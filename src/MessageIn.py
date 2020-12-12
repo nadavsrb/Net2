afaf4963133503f32f6b335cd55b5166cb5d56c1
yoav amar <yoav.6hutc@gmail.com>
2020-12-12 15:41:19 +0200

    added notes to message in

--- src/MessageIn.py
+++ src/MessageIn.py
@@ -1,10 +1,12 @@
+#this class is in charge of holding the data of the
+#massage from the client (the input).
 class MessageIn:
     STR_BEFORE_PATH = "GET "
     STR_AFTER_PATH = " HTTP/1.1"
     STR_BEFORE_CON = "Connection: "
     CLOSE_STR = "close"
     KEEP_STR = "keep-alive"
-
+    #the constructor gets the data as string and if there was a timeout.
     def __init__(self, data: str, isTimeout: bool):
         self.__isTimeout = isTimeout
         self.__isConClose = False
@@ -13,6 +15,7 @@ class MessageIn:
         self.__initialize(data)
 
     def __initialize(self, data: str):
+        #check if there was a timeout.
         if self.__isTimeout:
             return
 
@@ -22,26 +25,27 @@ class MessageIn:
         startPathIndex = data.find(self.STR_BEFORE_PATH) + len(self.STR_BEFORE_PATH)
         endPathIndex = data.find(self.STR_AFTER_PATH)
         startConIndex = data.find(self.STR_BEFORE_CON) + len(self.STR_BEFORE_CON)
-
+        #if one of the parts that the message should include wasn't found disconnect from the client.
         if startPathIndex == -1 or endPathIndex == -1 or startConIndex == -1:
             # Client sent a non valid message, disconnecting from client...
             self.__isTimeout = True
             return
-
+        #check the connection status.
         if data.startswith(self.CLOSE_STR, startConIndex):
             self.__isConClose = True
         elif not data.startswith(self.KEEP_STR, startConIndex):  # isConClose = False in default
             # Client sent a non valid message, disconnecting from client...
             self.__isTimeout = True
             return
-
+        #gets the file path from the input.
         self.__filePath = data[startPathIndex:endPathIndex]
 
+    #this func returns if there was a timeout
     def getIsTimeout(self):
         return self.__isTimeout
-
+    #this func returns if the connection is close.
     def getIsConClose(self):
         return self.__isConClose
-
+    #this func returns the path to the file.
     def getFilePath(self):
         return self.__filePath
