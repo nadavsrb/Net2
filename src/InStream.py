6513a8a6064011179c5ad3fe814bb6b0826c4069
yoav amar <yoav.6hutc@gmail.com>
2020-12-12 16:22:13 +0200

    added notes to input stream

--- src/InStream.py
+++ src/InStream.py
@@ -3,23 +3,27 @@ import socket
 from IOStream import IOStream
 from MessageIn import MessageIn
 
-
+#this class is in charge of handling the input from the client socket.
 class InStream(IOStream):
     TIMEOUT_SEC = 1.0
     BUFFER_SIZE = 100
-
+    #gets the client socket in the constructor.
     def __init__(self, clSocket: socket.socket):
         self.__clSocket = clSocket
         self.__nextData = ""
-
+    #get a message from the client and return an input message.
+    #add the data from before (the next data) to the data
+    #and reset the next data to empty string.
     def getMessage(self) -> MessageIn:
         isTimeout = False
         data = self.__nextData
         self.__nextData = ""
 
         try:
-
+            #until we haven't reach to the end of the massage we keep reading
+            #from the socket.
             while not (self.EOM in data):
+                #set a timeout to the client socket.
                 self.__clSocket.settimeout(self.TIMEOUT_SEC)
 
                 bytesData = self.__clSocket.recv(self.BUFFER_SIZE)
@@ -31,13 +35,14 @@ class InStream(IOStream):
                 self.__clSocket.settimeout(None)
 
             index = data.find(self.EOM) + len(self.EOM)
-
+            #if there is more input after the EOM we save it in the next data
+            #and save the data before the EOM in the data.
             if index < len(data):
                 self.__nextData = data[index:]
 
             data = data[:index]
-
+        #symbols that we reach the timeout.
         except socket.timeout:
             isTimeout = True
-
+        #return the message out according to the data and if there was a timeout.
         return MessageIn(data, isTimeout)
